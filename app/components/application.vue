0c6ff2ab36aa8d8c921c34c7c66a4d2a309a3006
Kate Leslie <thisizk8@gmail.com>
2017-03-03 20:41:22 -0600

    checked for syntax errors, didn't find relevant ones

--- app/components/application.vue
+++ app/components/application.vue
@@ -1,49 +1,61 @@
 <template lang="html">
   <div class="">
     <div class="modal">filler content</div>
-    <div class="container">
-    <Nav>Marvel</Nav>
-    <form action="search"><input type="search" name="" value=""></form>
-    <aside class="">
-      <img src="http://lorempixel.com/200/200/cats/" alt="">
-      <h3>Spider Girl</h3>
-      <p>2010 - 2011</p>
-      <h2>Creators</h2>
-      <ul>
-        <li>Tom Brennan</li>
-        <li>Mike Del Mundo</li>
-        <li>Clayton Henry</li>
-        <li>Barry Kitson</li>
-        <li>Jelena Kevic Djurdjevic</li>
-        <li>Paul Tobin</li>
-      </ul>
+      <div class="container">
+        <nav>Marvel</nav>
+        <form action="search">
+          <input type="search" name="" value="" />
+        </form>
+        <aside class="">
+          <img src="http://lorempixel.com/200/200/cats/" alt="">
+          <h3>Spider Girl</h3>
+          <p>2010 - 2011</p>
+          <h2>Creators</h2>
+          <ul>
+            <li>Tom Brennan</li>
+            <li>Mike Del Mundo</li>
+            <li>Clayton Henry</li>
+            <li>Barry Kitson</li>
+            <li>Jelena Kevic Djurdjevic</li>
+            <li>Paul Tobin</li>
+          </ul>
+        </aside>
 
-
-    </aside>
-
-    <h1>Characters</h1>
-    <div class="upper-character__container">
-    <div>
-    <img src="http://lorempixel.com/100/100/cats/" alt="">
-    <p>Red Hulk</p>
-    </div>
-    <div>
-    <img src="http://lorempixel.com/100/100/cats/" alt="">
-    <p>Red Hulk</p>
-    </div>
-    <div>
-    <img src="http://lorempixel.com/100/100/cats/" alt="">
-    <p>Red Hulk</p>
-    </div>
-    <div>
-    <img src="http://lorempixel.com/100/100/cats/" alt="">
-    <p>Red Hulk</p>
-    </div>
+        <h1>Characters</h1>
+        <div class="upper-character__container"><div>
+        <img src="http://lorempixel.com/100/100/cats/" alt="">
+        <p>Red Hulk</p>
+      </div>
+      <div>
+        <img src="http://lorempixel.com/100/100/cats/" alt="">
+        <p>Red Hulk</p>
+      </div>
+      <div>
+        <img src="http://lorempixel.com/100/100/cats/" alt="">
+        <p>Red Hulk</p>
+      </div>
+      <div>
+        <img src="http://lorempixel.com/100/100/cats/" alt="">
+        <p>Red Hulk</p>
+      </div>
     </div>
 
     <h1>Comics</h1>
-    <div class="lower-character__container">
-    <div>
+    <div class="lower-character__container"><div>
+    <img src="http://lorempixel.com/100/100/cats/" alt="">
+    <p>#8</p>
+    <p>Spider-Girl</p>
+    <p>(2010)#8</p>
+    <button type="button" name="button">Read More</button>
+  </div>
+  <div>
+    <img src="http://lorempixel.com/100/100/cats/" alt="">
+    <p>#8</p>
+    <p>Spider-Girl</p>
+    <p>(2010)#8</p>
+    <button type="button" name="button">Read More</button>
+  </div>
+  <div>
     <img src="http://lorempixel.com/100/100/cats/" alt="">
     <p>#8</p>
     <p>Spider-Girl</p>
@@ -51,33 +63,15 @@
     <button type="button" name="button">Read More</button>
     </div>
     <div>
-    <img src="http://lorempixel.com/100/100/cats/" alt="">
-    <p>#8</p>
-    <p>Spider-Girl</p>
-    <p>(2010)#8</p>
-    <button type="button" name="button">Read More</button>
+      <img src="http://lorempixel.com/100/100/cats/" alt="">
+      <p>#8</p>
+      <p>Spider-Girl</p>
+      <p>(2010)#8</p>
+      <button type="button" name="button">Read More</button>
     </div>
-    <div>
-    <img src="http://lorempixel.com/100/100/cats/" alt="">
-    <p>#8</p>
-    <p>Spider-Girl</p>
-    <p>(2010)#8</p>
-    <button type="button" name="button">Read More</button>
-    </div>
-    <div>
-    <img src="http://lorempixel.com/100/100/cats/" alt="">
-    <p>#8</p>
-    <p>Spider-Girl</p>
-    <p>(2010)#8</p>
-    <button type="button" name="button">Read More</button>
-    </div>
-    </div>
-
-
-
-
   </div>
   </div>
+</div>
 </template>
 
 <script>
